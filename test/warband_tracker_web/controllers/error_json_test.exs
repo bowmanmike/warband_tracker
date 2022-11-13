2a264dfb86cda53f45013b1043a559451cd953e0
Mike Bowman <mike@mikebowman.dev>
2022-11-13 12:09:25 -0500

    Format

--- test/warband_tracker_web/controllers/error_json_test.exs
+++ test/warband_tracker_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule WarbandTrackerWeb.ErrorJSONTest do
   use WarbandTrackerWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert WarbandTrackerWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert WarbandTrackerWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do
